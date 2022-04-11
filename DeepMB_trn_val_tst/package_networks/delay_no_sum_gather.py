0fb3d26216bf3ce106955252f82a7085f4f5983e
Guillaume Zahnd <guillaumezed@gmail.com>
2022-04-11 10:45:05 +0200

    Changes indices "y" to "z" for consistency

--- DeepMB_trn_val_tst/package_networks/delay_no_sum_gather.py
+++ DeepMB_trn_val_tst/package_networks/delay_no_sum_gather.py
@@ -11,7 +11,7 @@ class DelayNoSumGather(nn.Module):
 
     # Apparatus to compute the ToF given a specified SoS
     self.tof = ToF(transducer_pixel_distances, scanner_sampling_frequency, samples_offset)
-    self.nb_pixels_y, self.nb_pixels_x, self.nb_elements = self.tof.transducer_pixel_distances.shape
+    self.nb_pixels_z, self.nb_pixels_x, self.nb_elements = self.tof.transducer_pixel_distances.shape
 
 
   def forward(self, input_sinogram, sos):
@@ -22,10 +22,10 @@ class DelayNoSumGather(nn.Module):
     tof_floor, tof_ceil, tof_floor_factor, tof_ceil_factor = self.tof.cast_tof_for_linear_itp(self.tof.compute_tof(sos))
 
     # Reshape and initialize
-    tof_floor = tof_floor.view(batch_size, self.nb_pixels_y * self.nb_pixels_x, self.nb_elements)
-    tof_ceil = tof_ceil.view(batch_size, self.nb_pixels_y * self.nb_pixels_x, self.nb_elements)
-    tof_floor_factor = tof_floor_factor.view(batch_size, self.nb_pixels_y * self.nb_pixels_x, self.nb_elements)
-    tof_ceil_factor = tof_ceil_factor.view(batch_size, self.nb_pixels_y * self.nb_pixels_x, self.nb_elements)
+    tof_floor = tof_floor.view(batch_size, self.nb_pixels_z * self.nb_pixels_x, self.nb_elements)
+    tof_ceil = tof_ceil.view(batch_size, self.nb_pixels_z * self.nb_pixels_x, self.nb_elements)
+    tof_floor_factor = tof_floor_factor.view(batch_size, self.nb_pixels_z * self.nb_pixels_x, self.nb_elements)
+    tof_ceil_factor = tof_ceil_factor.view(batch_size, self.nb_pixels_z * self.nb_pixels_x, self.nb_elements)
 
     # Remove singleton channel dimension
     input_sinogram = input_sinogram.reshape(batch_size, -1, self.nb_elements)
@@ -37,6 +37,6 @@ class DelayNoSumGather(nn.Module):
 
     # Reshape to image-like output with transducers as channels
     ima = ima.view((
-      batch_size, self.nb_pixels_y, self.nb_pixels_x, self.nb_elements)).permute(0, 3, 1, 2).contiguous().float()
+      batch_size, self.nb_pixels_z, self.nb_pixels_x, self.nb_elements)).permute(0, 3, 1, 2).contiguous().float()
 
     return ima
