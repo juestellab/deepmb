30d1a7627a29d547ea02a8b137c5abb38bfebbc7
gzahnd <zahnd.guillaume@gmail.com>
2022-09-08 11:24:43 +0200

    Creation of an orphan branch to store binaries

--- DeepMB_trn_val_tst/package_utils/create_folder_if_not_exist.py
+++
@@ -1,6 +0,0 @@
-import os
-
-
-def create_folder_if_not_exist(folder):
-  if not os.path.exists(folder):
-    os.makedirs(folder)
--- DeepMB_trn_val_tst/package_utils/stopwatch.py
+++
@@ -1,9 +0,0 @@
-import math
-
-
-def stopwatch(time_now, time_ini):
-  time_native = time_now - time_ini
-  time_minutes = int(time_native // 60)
-  time_seconds = int(time_native % 60)
-  time_milliseconds = int(1000*(time_native - math.floor(time_native)))
-  return str(time_minutes).zfill(2) + ':' + str(time_seconds).zfill(2) + ':' + str(time_milliseconds).zfill(3)
